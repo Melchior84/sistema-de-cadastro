7797be777ae9c589f5cd57407f215a1d7a361e81
MELCHIOR PASSOS ARAUJO <melchior@previ.com.br>
2022-11-21 10:12:37 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectando ao bando de dados")
+console.log("O Paulo fez esse commit sem voce ver na aula!")
+console.log("Enviando para o git hub")
